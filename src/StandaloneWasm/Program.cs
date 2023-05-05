90bedb0077a708be5280777cb1bee1b1dd569c2f
Justin Yoo <justin.yoojh@gmail.com>
2023-05-05 18:11:59 +0900

    Add standalone WASM app

---
+++ src/StandaloneWasm/Program.cs
@@ -0,0 +1,16 @@
+using Common.Services;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using StandaloneWasm;
+using StandaloneWasm.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+
+await builder.Build().RunAsync();
