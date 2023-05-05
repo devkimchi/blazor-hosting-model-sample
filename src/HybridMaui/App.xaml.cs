dd525f105215cae5d596c1e236741106087b1fb0
Justin Yoo <justin.yoojh@gmail.com>
2023-05-05 19:03:47 +0900

    Add Blazor Hybrid MAUI app

---
+++ src/HybridMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HybridMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
